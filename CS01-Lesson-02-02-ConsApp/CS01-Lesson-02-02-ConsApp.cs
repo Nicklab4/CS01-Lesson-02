c2738d43afb9ed107cba3b6b7022506b1f1c15fb
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-03 01:11:18 +0500

    Commit практического задания №2

---
+++ CS01-Lesson-02-02-ConsApp/CS01-Lesson-02-02-ConsApp.cs
@@ -0,0 +1,31 @@
+﻿//Задача 2.
+//Сумма двух последних цифр.
+//Вывести в диапазоне от 10 до 100 все числа,
+//сумма двух последних цифр которых равна 10:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_02_02_ConsApp
+{
+    class Program
+    {
+        static bool Check(int a)
+        {
+            if ((a % 10 + a / 10 % 10 == 10))
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 100; i++)
+                if (Check(i)) Console.WriteLine(i);
+
+            Console.ReadLine();
+        }
+    }
+}
