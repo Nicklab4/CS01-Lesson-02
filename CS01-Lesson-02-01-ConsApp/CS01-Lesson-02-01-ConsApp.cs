c38f60df169b84e66d68db967f99124b98e69d46
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-03 00:59:49 +0500

    Commit практического задания №1

--- CS01-Lesson-02-01-ConsApp/CS01-Lesson-02-01-ConsApp.cs
+++ CS01-Lesson-02-01-ConsApp/CS01-Lesson-02-01-ConsApp.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Задача 1.
+//Алгоритм нахождения НОД и организация метода
+//Реализовать метод нахождения NOD, используя алгоритм Евклида:
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +11,22 @@ namespace CS01_Lesson_02
 {
     class Program
     {
+        static int NOD(int a, int b)
+        {
+            while (a != b)
+                if (a > b) a = a - b;
+                else b = b - a;
+            return a;
+        }
+
         static void Main(string[] args)
         {
+            int a = 524;
+            int b = 224;
+
+            Console.WriteLine(NOD(a, b));
+
+            Console.ReadLine();
         }
     }
 }
