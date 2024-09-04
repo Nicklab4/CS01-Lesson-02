78c5a06c8e94fc60a48b79381c35bb26738ce94b
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-04 23:52:51 +0500

    Commit практического задания №5

---
+++ CS01-Lesson-02-05-ConsApp/CS01-Lesson-02-05-ConsApp.cs
@@ -0,0 +1,43 @@
+﻿//Задача 5. Учимся подсчитывать эффективность программы
+//   Для этого давайте научимся подсчитывать время выполнения программы.
+// Решим задачу нахождения простых чисел в диапазоне от 1 до 1000000.
+// Напишем метод проверки, является ли число простым, и используем его
+// для подсчёта количества чисел. В начале цикла сохраним текущее время,
+// по выходу из цикла вычтем текущее время из сохранённого и выведем
+// результат на экран.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_02_05_ConsApp
+{
+    class Program
+    {
+        static bool IsSImple(int n)
+        {
+            //for (int i = 2; i <= n / 2; i++)
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now;
+            int k = 0;
+            for (int i = 2 ; i < 1000000; i++)
+                if (IsSImple(i))
+                {
+                    k++;
+                    Console.WriteLine("{0} {1}", k, i);
+                }
+
+            Console.WriteLine(k);
+            Console.WriteLine(DateTime.Now - start);
+            Console.ReadLine();
+        }
+    }
+}
