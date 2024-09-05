4c4bde9cf66f04d82d22324d3e34bde29e03ace2
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-05 23:54:31 +0500

    Commit практического задания №8

---
+++ CS01-Lesson-02-08-ConsApp/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 8. «Ханойская башня»
+//Реализовать на C# рекурсивный алгоритм игры «Ханойская башня»:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_02_08_ConsApp
+{
+    class Program
+    {
+        static void Move(int number, int from, int to, int free)
+        {
+            if (number > 0)
+            {
+                Move(number - 1, from, free, to);
+                Console.WriteLine("{0} => {1}", from, to);
+                Move(number - 1, free, to, from);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Move(4, 1, 2, 3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CS01-Lesson-02-HW01-ConsApp/CS01-Lesson-02-HW01-ConsApp.cs
@@ -0,0 +1,16 @@
+﻿// 1. Написать метод, возвращающий минимальное из трёх чисел.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_02_HW01_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
