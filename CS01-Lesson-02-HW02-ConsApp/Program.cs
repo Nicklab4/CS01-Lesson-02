63924e37ea0c73714ff7eb39b160bae51d730a67
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-09 14:07:55 +0500

    Commit домашнего задания №2

---
+++ CS01-Lesson-02-HW02-ConsApp/Program.cs
@@ -0,0 +1,28 @@
+﻿//2. Написать метод подсчета количества цифр числа.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_02_HW02_ConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            decimal x = decimal.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (x > 0)
+            {
+                count++;
+                x = x / 10;
+            }
+
+            Console.WriteLine("Ваше число содержит {0} символов", count);
+            Console.ReadLine();
+        }
+    }
+}
