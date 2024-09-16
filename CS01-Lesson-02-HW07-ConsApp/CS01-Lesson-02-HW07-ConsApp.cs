e0c6a5ac21d1e3893c3994e766ae3ba14b939b5f
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2024-09-16 22:23:25 +0500

    Commit домашнего задания №7

---
+++ CS01-Lesson-02-HW07-ConsApp/CS01-Lesson-02-HW07-ConsApp.cs
@@ -0,0 +1,59 @@
+﻿//7. n) Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).
+//   б) *Разработать рекурсивный метод, который считает сумму чисел от a до b.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CS01_Lesson_02_HW07_Consnpp
+{
+    class Progrnm
+    {
+        // Рекурсивный метод, который выводящий на экран числа от n до m(n<b).
+        static int loopRec(int n , int m)
+        {
+            // Реверсия чисел если n больше m
+            if (n > m)
+            {
+                n = n + m;
+                m = n - m;
+                n = n - m;
+            }
+
+            if (n == m)
+            {
+                Console.WriteLine(n);
+                return n;
+            }
+            Console.WriteLine(n);
+            return loopRec(++n, m);
+        }
+
+        // Рекурсивный метод, который считает сумму чисел от n до m.
+        static int sumRec(int n, int m)
+        {
+            // Реверсия чисел если n больше m
+            if (n > m)
+            {
+                n = n + m;
+                m = n - m;
+                n = n - m;
+            }
+
+            if (n == m) return n;
+            return (n + sumRec(++n, m));
+        }
+
+        static void Main(string[] args)
+        {
+            int a=10, b=-3;
+
+            loopRec(a, b);
+            Console.WriteLine("Сумма всех чисел: {0}", sumRec(a, b));
+
+            Console.ReadLine();
+        }
+    }
+}
